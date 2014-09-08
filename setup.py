05d6fd57118292335e63295149bd7f9631cafc59
CJ Carey <perimosocordiae@gmail.com>
2014-09-08 16:18:49 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+version = "0.1.0"
+setup(name='metric_learn',
+      version=version,
+      description='Python implementations of metric learning algorithms',
+      author='CJ Carey',
+      author_email='ccarey@cs.umass.edu',
+      url='http://github.com/all-umass/metric_learn',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+      ],
+      packages=['metric_learn']
+      )
+
