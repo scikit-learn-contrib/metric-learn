1a82f7dde6be5777c33055b1b019bdf01badd975
Alex Seewald <seewalker.120@gmail.com>
2015-11-07 17:03:51 -0500

    Modified metric_learn/__init__.py to fail with more informative error when importing metric_learn is attempted with python 3.*

--- metric_learn/__init__.py
+++ metric_learn/__init__.py
@@ -1,3 +1,7 @@
+import sys
+if sys.version_info >= (3, 0):
+    sys.stderr.write("metric-learn works with python 2.6+, but not python 3.*. Exiting.")
+    sys.exit(1)
 from itml import ITML
 from lmnn import LMNN
 from lsml import LSML
