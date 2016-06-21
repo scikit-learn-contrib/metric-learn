d48e4e7716f6616f062eeff18998df4cbbdc3ae4
Ondřej Švec <ond.svec@gmail.com>
2016-06-21 21:27:07 +0200

    Simple Covariance metric-learner (#21)
    * Simple Covariance metric-learner
    
    * Updated docstring

---
+++ metric_learn/covariance.py
@@ -0,0 +1,29 @@
+"""
+Covariance metric (baseline method)
+
+This method does not "learn" anything, only calculates covariance matrix.
+Inverse of this covariance matrix is then used in Mahalanobis distance.
+
+This is a simple baseline method first introduced in
+On the Generalized Distance in Statistics, P.C.Mahalanobis, 1936
+"""
+
+from __future__ import absolute_import
+import numpy as np
+from .base_metric import BaseMetricLearner
+
+
+class Covariance(BaseMetricLearner):
+  def __init__(self):
+    self.params = {}
+
+  def metric(self):
+    return self.M
+
+  def fit(self, X, y=None):
+    """
+    X: data matrix, (n x d)
+    y: labels (optional)
+    """
+    self.M = np.cov(X.T)
+    return self
