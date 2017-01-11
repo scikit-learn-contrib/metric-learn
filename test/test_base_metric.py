11e95d62b03a25170199dd0788a79885ec301129
Ondřej Švec <ond.svec@gmail.com>
2017-01-11 18:08:12 +0100

    Added __repr__ to BaseMetricLearner (#47)
    * Added __repr__ to BaseMetricLearner
    * Added test for __repr__

---
+++ test/test_base_metric.py
@@ -0,0 +1,20 @@
+import unittest
+import numpy as np
+
+from metric_learn import (
+    LMNN, NCA, LFDA, Covariance, MLKR,
+    LSML_Supervised, ITML_Supervised, SDML_Supervised, RCA_Supervised)
+
+
+class TestBaseMetric(unittest.TestCase):
+
+  def test_string_repr(self):
+    '''
+    Test string representation of some of the learning methods.
+    '''
+    self.assertEqual(str(Covariance()), "Covariance()")
+    self.assertEqual(str(NCA()), "NCA(learning_rate=0.01, max_iter=100, num_dims=None)")
+    self.assertEqual(str(LFDA()), "LFDA(dim=None, k=7, metric='weighted')")
+
+if __name__ == '__main__':
+  unittest.main()
