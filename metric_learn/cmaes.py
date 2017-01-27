9d2dc0ef52d15fa01b1e602fe179eab468ef43ec
svecon <ond.svec@gmail.com>
2017-01-27 11:58:34 +0100

    Added Differential evolution

--- metric_learn/cmaes.py
+++ metric_learn/cmaes.py
@@ -261,7 +261,7 @@ class CMAES(BaseMetricLearner, _BaseBuilder):
     CMAES
     '''
     def __init__(self, classifier='knn', transformer='full', n_gen=25, class_separation=False,
-                 train_subset_size=1.0, split_size=0.33,
+                 train_subset_size=1.0, split_size=0.33, evolution_strategy='cmaes',
                  random_state=None, verbose=False, **kwargs):
         """Initialize the learner.
 
@@ -285,6 +285,7 @@ class CMAES(BaseMetricLearner, _BaseBuilder):
         self.params = {
             **kwargs,
             'classifier': classifier,
+            'evolution_strategy': evolution_strategy,
             'transformer': transformer,
             'n_gen': n_gen,
             'class_separation': class_separation,
@@ -293,6 +294,7 @@ class CMAES(BaseMetricLearner, _BaseBuilder):
             'random_state': random_state,
             'verbose': verbose,
         }
+        np.random.seed(random_state)
 
     def transform(self, X):
         return self._transformer.transform(X)
@@ -342,12 +344,94 @@ class CMAES(BaseMetricLearner, _BaseBuilder):
          X: (n, d) array-like of samples
          Y: (n,) array-like of class labels
         '''
-        np.random.seed(self.params['random_state'])
-        self._transformer = self._build_transformer(self.params['transformer'])
-
-        sizeOfIndividual = self._transformer.individual_size(X.shape[1])
+        if self.params['evolution_strategy']=='cmaes':
+            return self.fit_cmaes(X, Y)
         
-        strategy = cma.Strategy(centroid=[0.0]*sizeOfIndividual, sigma=1.0)
+        return self.fit_diffevo(X, Y)
+
+    def fit_diffevo(self, X, Y):
+        '''
+         X: (n, d) array-like of samples
+         Y: (n,) array-like of class labels
+        '''
+        self._transformer = self._build_transformer(self.params['transformer'])
+        individual_size = self._transformer.individual_size(X.shape[1])
+        
+        toolbox.register("attr_float", np.random.uniform, -1, 1)
+        toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, individual_size)
+        toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+        toolbox.register("select", tools.selRandom, k=3)
+        
+        toolbox.register("evaluate", self.evaluation_builder(X, Y))
+
+        self.hof = tools.HallOfFame(1)
+        
+        if self.params['verbose']:
+            stats_fit = tools.Statistics(key=lambda ind: ind.fitness.values)
+            stats_fit.register("avg", np.mean, axis=0)
+            stats_fit.register("std", np.std, axis=0)
+            stats_fit.register("min", np.min, axis=0)
+            stats_fit.register("max", np.max, axis=0)
+            
+            stats_size = tools.Statistics()
+            stats_size.register("x", lambda x: x)
+
+            stats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+            stats = stats_fit
+        else:
+            stats=None
+
+        # Differential evolution parameters
+        CR = 0.25
+        F = 1  
+        MU = 50
+        
+        pop = toolbox.population(n=MU);
+        
+        logbook = tools.Logbook()
+        logbook.header = "gen", "evals", "std", "min", "avg", "max"
+        
+        # Evaluate the individuals
+        fitnesses = toolbox.map(toolbox.evaluate, pop)
+        for ind, fit in zip(pop, fitnesses):
+            ind.fitness.values = fit
+        
+        if stats:
+            record = stats.compile(pop)
+            logbook.record(gen=0, evals=len(pop), **record)
+            print(logbook.stream)
+        
+        for g in range(1, self.params['n_gen']):
+            for k, agent in enumerate(pop):
+                a,b,c = toolbox.select(pop)
+                y = toolbox.clone(agent)
+                index = np.random.randint(individual_size)
+                for i, value in enumerate(agent):
+                    if i == index or np.random.random() < CR:
+                        y[i] = a[i] + F*(b[i]-c[i])
+                y.fitness.values = toolbox.evaluate(y)
+                if y.fitness > agent.fitness:
+                    pop[k] = y
+            self.hof.update(pop)
+            
+            if stats:
+                record = stats.compile(pop)
+                logbook.record(gen=g, evals=len(pop), **record)
+                print(logbook.stream)
+                print("Best individual is ", self.hof[0], self.hof[0].fitness.values[0])
+        
+        self._transformer.fit(X, Y, self.hof[0])
+        return self
+
+    def fit_cmaes(self, X, Y):
+        '''
+         X: (n, d) array-like of samples
+         Y: (n,) array-like of class labels
+        '''
+        self._transformer = self._build_transformer(self.params['transformer'])
+        individual_size = self._transformer.individual_size(X.shape[1])
+        
+        strategy = cma.Strategy(centroid=[0.0]*individual_size, sigma=1.0)
         toolbox.register("evaluate", self.evaluation_builder(X, Y))
         toolbox.register("generate", strategy.generate, creator.Individual)
         toolbox.register("update", strategy.update)
