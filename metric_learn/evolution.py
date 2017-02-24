cfbdeb80e9c4ae65f24ba042a4aa715e8eb0f8e9
svecon <ond.svec@gmail.com>
2017-02-24 12:27:38 +0100

    Added WeightedPurity fitness

--- metric_learn/evolution.py
+++ metric_learn/evolution.py
@@ -93,6 +93,39 @@ class RandomFitness(BaseFitness):
     def __call__(self, X_train, X_test, y_train, y_test):
         return np.random.random()
 
+class WeightedPurityFitness(BaseFitness):
+    def __init__(self, sig=5, kmeans__n_init=1):
+        self.params = {
+            'sig': sig,
+            'kmeans__n_init': kmeans__n_init,
+        }
+
+    @staticmethod
+    def available(method):
+        return method in ['wpur']
+
+    def __call__(self, X_train, X_test, y_train, y_test):
+        X = np.vstack([X_train, X_test])
+        y = np.hstack([y_train, y_test])
+        le = LabelEncoder()
+        y = le.fit_transform(y)
+
+        kmeans = KMeans(n_clusters=len(np.unique(y)), n_init=self.params['kmeans__n_init'])
+        kmeans.fit(X)
+
+        r = distance.cdist(kmeans.cluster_centers_, kmeans.cluster_centers_)
+        h = np.exp(-r/(self.params['sig']**2))
+
+        N = confusion_matrix(y, kmeans.labels_)
+
+        wN = np.zeros(h.shape)
+        for l in range(wN.shape[0]): # label
+            for c in range(wN.shape[0]): # cluster
+                for j in range(wN.shape[0]):
+                    wN[l,c] += h[l,c]*N[l,j]
+
+        return wN.max(axis=0).sum() / wN.sum()
+
 class WeightedFMeasureFitness(BaseFitness):
     def __init__(self, weighted=False, sig=5, kmeans__n_init=1):
         self.params = {
@@ -821,8 +854,11 @@ class MetricEvolution(BaseMetricLearner, BaseBuilder):
         if ClassSeparationFitness.available(fitness):
             return ClassSeparationFitness()
 
+        if WeightedPurityFitness.available(fitness):
+            return WeightedPurityFitness(**params)
+
         if WeightedFMeasureFitness.available(fitness):
-            return WeightedFMeasureFitness()
+            return WeightedFMeasureFitness(**params)
 
          # TODO unify error messages
         raise ValueError('Invalid value of fitness: `{}`'.format(fitness))
