e458cb22d60e0a5a7589acff0e90004cd2e4dc4a
svecon <ond.svec@gmail.com>
2017-04-18 17:25:38 +0200

    Triangular transformer

--- metric_learn/evolution.py
+++ metric_learn/evolution.py
@@ -216,6 +216,8 @@ class BaseBuilder():
             return DiagonalMatrixTransformer(**params)
         elif transformer == 'full':
             return FullMatrixTransformer(**params)
+        elif transformer == 'triangular':
+            return TriangularMatrixTransformer(**params)
         elif transformer == 'neuralnetwork':
             return NeuralNetworkTransformer(**params)
         elif transformer == 'kmeans':
@@ -256,6 +258,23 @@ class DiagonalMatrixTransformer(MatrixTransformer):
                 .format(input_dim, len(flat_weights)))
 
         self.L = np.diag(flat_weights)
+        return self
+
+class TriangularMatrixTransformer(MatrixTransformer):
+    def __init__(self):
+        self.params = {}
+
+    def individual_size(self, input_dim):
+        return input_dim*(input_dim+1)//2
+
+    def fit(self, X, y, flat_weights):
+        input_dim = X.shape[1]
+        if self.individual_size(input_dim) != len(flat_weights):
+            raise Exception('`input_dim` and `flat_weights` sizes do not match: {} vs {}'
+                .format(self.individual_size(input_dim), len(flat_weights)))
+
+        self.L = np.zeros((input_dim, input_dim))
+        self.L[np.tril_indices(input_dim, 0)] = flat_weights
         return self
 
 class FullMatrixTransformer(MatrixTransformer):
