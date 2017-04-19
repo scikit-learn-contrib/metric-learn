7d1c694064fa40eff16a52509b6ecb808686c8a0
svecon <ond.svec@gmail.com>
2017-04-19 13:42:40 +0200

    Expose everything from MetricEvolution

--- metric_learn/__init__.py
+++ metric_learn/__init__.py
@@ -10,4 +10,4 @@ from .nca import NCA
 from .lfda import LFDA
 from .rca import RCA, RCA_Supervised
 from .mlkr import MLKR
-from .evolution import MetricEvolution, DiagonalMatrixTransformer, FullMatrixTransformer, NeuralNetworkTransformer
+from .evolution import *
