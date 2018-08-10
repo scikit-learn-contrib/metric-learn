FIX: fix numerical problems in MLKR with logsumexp

metric_learn/mlkr.py
@@ -8,6 +8,7 @@ for dimensionality reduction and high dimensional data visualization.
 """
 from __future__ import division, print_function
 import numpy as np
+from sklearn.utils.fixes import logsumexp
 from scipy.optimize import minimize
 from scipy.spatial.distance import pdist, squareform
 from sklearn.decomposition import PCA
@@ -97,15 +98,15 @@ class MLKR(BaseMetricLearner):
 def _loss(flatA, X, y, dX):
   A = flatA.reshape((-1, X.shape[1]))
   dist = pdist(X, metric='mahalanobis', VI=A.T.dot(A))
-  K = squareform(np.exp(-dist**2))
-  np.fill_diagonal(K, 0)
-  denom = np.maximum(K.sum(axis=0), EPS)
-  yhat = K.dot(y) / denom
+  dist = squareform(dist ** 2)
+  np.fill_diagonal(dist, np.inf)
+  softmax = np.exp(- dist - logsumexp(- dist, axis=1)[:, np.newaxis])
+  yhat = softmax.dot(y)
   ydiff = yhat - y
   cost = (ydiff**2).sum()
 
   # also compute the gradient
-  W = 2 * K * ((ydiff / denom)[:, np.newaxis] * (yhat[:, np.newaxis] - y))
+  W = 2 * softmax * (ydiff[:, np.newaxis] * (yhat[:, np.newaxis] - y))
   # note: this is the part that the matlab impl drops to C for
   M = (dX.T * W.ravel()).dot(dX)
   grad = 2 * A.dot(M)