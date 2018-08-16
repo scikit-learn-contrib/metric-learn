[MRG] FIX: fix MLKR cost and gradient (#111)
* FIX: fix mlkr cost and gradient

* FIX: fix numerical problems in MLKR with logsumexp

* STY: remove useless parenthesis

* ENH: improve MLKR memory performance

metric_learn/mlkr.py
@@ -8,6 +8,7 @@ for dimensionality reduction and high dimensional data visualization.
 """
 from __future__ import division, print_function
 import numpy as np
+from sklearn.utils.fixes import logsumexp
 from scipy.optimize import minimize
 from scipy.spatial.distance import pdist, squareform
 from sklearn.decomposition import PCA
@@ -79,11 +80,7 @@ class MLKR(BaseMetricLearner):
       """
       X, y, A = self._process_inputs(X, y)
 
-      # note: this line takes (n*n*d) memory!
-      # for larger datasets, we'll need to compute dX as we go
-      dX = (X[None] - X[:, None]).reshape((-1, X.shape[1]))
-
-      res = minimize(_loss, A.ravel(), (X, y, dX), method='CG', jac=True,
+      res = minimize(_loss, A.ravel(), (X, y), method='CG', jac=True,
                      tol=self.alpha,
                      options=dict(maxiter=self.max_iter, eps=self.epsilon))
       self.transformer_ = res.x.reshape(A.shape)
@@ -94,19 +91,18 @@ class MLKR(BaseMetricLearner):
       return self.transformer_
 
 
-def _loss(flatA, X, y, dX):
+def _loss(flatA, X, y):
   A = flatA.reshape((-1, X.shape[1]))
   dist = pdist(X, metric='mahalanobis', VI=A.T.dot(A))
-  K = squareform(np.exp(-dist**2))
-  denom = np.maximum(K.sum(axis=0), EPS)
-  yhat = K.dot(y) / denom
+  dist = squareform(dist ** 2)
+  np.fill_diagonal(dist, np.inf)
+  softmax = np.exp(- dist - logsumexp(- dist, axis=1)[:, np.newaxis])
+  yhat = softmax.dot(y)
   ydiff = yhat - y
   cost = (ydiff**2).sum()
 
   # also compute the gradient
-  np.fill_diagonal(K, 1)
-  W = 2 * K * (np.outer(ydiff, ydiff) / denom)
-  # note: this is the part that the matlab impl drops to C for
-  M = (dX.T * W.ravel()).dot(dX)
-  grad = 2 * A.dot(M)
+  W = softmax * ydiff[:, np.newaxis] * (yhat[:, np.newaxis] - y)
+  X_emb_t = A.dot(X.T)
+  grad = 4 * (X_emb_t * W.sum(axis=0) - X_emb_t.dot(W + W.T)).dot(X)
   return cost, grad.ravel()