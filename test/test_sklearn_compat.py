d95b22a3d4500959285dbc564fc379257a2b1675
William de Vazelhes <william.de-vazelhes@inria.fr>
2018-11-20 18:00:34 +0100

    MAINT: remove _get_args copied from scikit-learn and replace it by an import

--- test/test_sklearn_compat.py
+++ test/test_sklearn_compat.py
@@ -18,6 +18,7 @@ from sklearn import clone
 import numpy as np
 from sklearn.model_selection import (cross_val_score, cross_val_predict,
                                      train_test_split)
+from sklearn.utils.testing import _get_args
 
 
 # Wrap the _Supervised methods with a deterministic wrapper for testing.
@@ -390,26 +391,6 @@ def test_dont_overwrite_parameters(estimator, build_dataset, preprocessor):
       " to change attributes started"
       " or ended with _, but"
       " %s changed" % ', '.join(attrs_changed_by_fit))
-
-
-def _get_args(function, varargs=False):
-    """Helper to get function arguments"""
-
-    try:
-        params = signature(function).parameters
-    except ValueError:
-        # Error on builtin C function
-        return []
-    args = [key for key, param in params.items()
-            if param.kind not in (param.VAR_POSITIONAL, param.VAR_KEYWORD)]
-    if varargs:
-        varargs = [param.name for param in params.values()
-                   if param.kind == param.VAR_POSITIONAL]
-        if len(varargs) == 0:
-            varargs = None
-        return args, varargs
-    else:
-        return args
 
 
 if __name__ == '__main__':
