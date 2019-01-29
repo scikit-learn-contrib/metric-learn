40b2c88e0f866c82db0741d50c57cfba3478d133
William de Vazelhes <william.de-vazelhes@inria.fr>
2019-01-29 12:12:44 +0100

    MAINT call the prior prior

--- metric_learn/sdml.py
+++ metric_learn/sdml.py
@@ -58,12 +58,12 @@ class _BaseSDML(MahalanobisMixin):
     # set up prior M
     if self.use_cov:
       X = np.vstack({tuple(row) for row in pairs.reshape(-1, pairs.shape[2])})
-      self.M_ = pinvh(np.cov(X, rowvar = False))
+      prior = pinvh(np.cov(X, rowvar = False))
     else:
-      self.M_ = np.identity(pairs.shape[2])
+      prior = np.identity(pairs.shape[2])
     diff = pairs[:, 0] - pairs[:, 1]
     loss_matrix = (diff.T * y).dot(diff)
-    emp_cov = pinvh(self.M_) + self.balance_param * loss_matrix
+    emp_cov = pinvh(prior) + self.balance_param * loss_matrix
     _, self.M_ = graph_lasso(emp_cov, self.sparsity_param, verbose=self.verbose)
 
     self.transformer_ = transformer_from_metric(self.M_)
