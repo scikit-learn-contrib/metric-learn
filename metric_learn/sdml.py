c57a35a8f7497de0bd0208049817b8540d2698d6
William de Vazelhes <william.de-vazelhes@inria.fr>
2019-02-01 14:09:21 +0100

    MAINT: make more explicit the fact that to use identity (i.e. an SPD matrix) as initialization

--- metric_learn/sdml.py
+++ metric_learn/sdml.py
@@ -64,7 +64,8 @@ class _BaseSDML(MahalanobisMixin):
     diff = pairs[:, 0] - pairs[:, 1]
     loss_matrix = (diff.T * y).dot(diff)
     emp_cov = pinvh(prior) + self.balance_param * loss_matrix
-    M, *_ = quic(emp_cov, lam=self.sparsity_param, msg=self.verbose)
+    M, *_ = quic(emp_cov, lam=self.sparsity_param, msg=self.verbose,
+                 Theta0=np.eye(pairs.shape[2]))
 
     self.transformer_ = transformer_from_metric(M)
     return self
