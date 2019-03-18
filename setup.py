a7ed1bbdf23907c5bbfc8a176e1214e6b0a2f63b
William de Vazelhes <william.de-vazelhes@inria.fr>
2019-03-18 15:29:12 +0100

    Specify skggm version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(name='metric-learn',
       extras_require=dict(
           docs=['sphinx', 'shinx_rtd_theme', 'numpydoc'],
           demo=['matplotlib'],
-          sdml=['skggm']
+          sdml=['skggm>=0.2.9']
       ),
       test_suite='test',
       keywords=[
