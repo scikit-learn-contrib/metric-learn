5edad14f7bd06436fa8d8bfa9062447e4b5d2972
William de Vazelhes <william.de-vazelhes@inria.fr>
2019-04-19 11:22:36 +0200

    MAINT: Address https://github.com/metric-learn/metric-learn/pull/193#pullrequestreview-228280763

--- metric_learn/_util.py
+++ metric_learn/_util.py
@@ -408,12 +408,10 @@ def validate_vector(u, dtype=None):
 
 
 def _check_num_dims(n_features, num_dims):
-  """Checks that num_dims is less that n_features and deal with the None
+  """Checks that num_dims is less than n_features and deal with the None
   case"""
   if num_dims is None:
-    dim = n_features
-  else:
-    if not 0 < num_dims <= n_features:
-      raise ValueError('Invalid num_dims, must be in [1, %d]' % n_features)
-    dim = num_dims
-  return dim
+    return n_features
+  if 0 < num_dims <= n_features:
+    return n_features
+  raise ValueError('Invalid num_dims, must be in [1, %d]' % n_features)
