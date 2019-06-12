6d6a38aaadf9ea685ed4a674d718604ebef7bed4
William de Vazelhes <william.de-vazelhes@inria.fr>
2019-06-12 16:37:31 +0200

    Address https://github.com/metric-learn/metric-learn/pull/194#discussion_r292387277

--- metric_learn/rca.py
+++ metric_learn/rca.py
@@ -107,18 +107,21 @@ class RCA(MahalanobisMixin, TransformerMixin):
                     DeprecationWarning)
 
     if self.pca_comps != 'deprecated':
-      warnings.warn('"pca_comps" parameter is not used.'
-                    ' It has been deprecated in version 0.5.0 and will be'
-                    'removed in 0.6.0', DeprecationWarning)
+      warnings.warn(
+        '"pca_comps" parameter is not used. '
+        'It has been deprecated in version 0.5.0 and will be'
+        'removed in 0.6.0. RCA will not do PCA preprocessing anymore. If '
+        'you still want to do it, you could use '
+        '`sklearn.decomposition.PCA` and an `sklearn.pipeline.Pipeline`.',
+        DeprecationWarning)
 
     X, chunks = self._prepare_inputs(X, chunks, ensure_min_samples=2)
 
-    warnings.warn("RCA will no longer be trained on a preprocessed version "
-                  "of the input as before. If you want to do some "
-                  "preprocessing, you should do it manually (you can also use "
-                  "an `sklearn.pipeline.Pipeline` for instance). This warning "
-                  "will disappear in version 0.6.0.",
-                  ChangedBehaviorWarning)
+    warnings.warn(
+      "RCA will no longer center the data before training. If you want "
+      "to do some preprocessing, you should do it manually (you can also "
+      "use an `sklearn.pipeline.Pipeline` for instance). This warning "
+      "will disappear in version 0.6.0.", ChangedBehaviorWarning)
 
     chunks = np.asanyarray(chunks, dtype=int)
     chunk_mask, chunked_data = _chunk_mean_centering(X, chunks)
--- test/metric_learn_test.py
+++ test/metric_learn_test.py
@@ -829,9 +829,11 @@ class TestRCA(MetricTestCase):
     # TODO: remove in v.0.6
     X, y = make_classification(random_state=42, n_samples=100)
     rca_supervised = RCA_Supervised(pca_comps=X.shape[1], num_chunks=20)
-    msg = ('"pca_comps" parameter is not used.'
-           ' It has been deprecated in version 0.5.0 and will be'
-           'removed in 0.6.0')
+    msg = ('"pca_comps" parameter is not used. '
+           'It has been deprecated in version 0.5.0 and will be'
+           'removed in 0.6.0. RCA will not do PCA preprocessing anymore. If '
+           'you still want to do it, you could use '
+           '`sklearn.decomposition.PCA` and an `sklearn.pipeline.Pipeline`.')
     with pytest.warns(ChangedBehaviorWarning) as expected_msg:
       rca_supervised.fit(X, y)
     assert str(expected_msg[0].message) == msg
@@ -845,10 +847,9 @@ class TestRCA(MetricTestCase):
     # test that a ChangedBehaviorWarning is thrown when using RCA
     # TODO: remove in v.0.6
 
-    msg = ("RCA will no longer be trained on a preprocessed version "
-           "of the input as before. If you want to do some "
-           "preprocessing, you should do it manually (you can also use "
-           "an `sklearn.pipeline.Pipeline` for instance). This warning "
+    msg = ("RCA will no longer center the data before training. If you want "
+           "to do some preprocessing, you should do it manually (you can also "
+           "use an `sklearn.pipeline.Pipeline` for instance). This warning "
            "will disappear in version 0.6.0.")
 
     X, y = make_classification(random_state=42, n_samples=100)
