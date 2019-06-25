bdaa890e5c7dd22a77bc2ddc6d9d29c5ff5e34f8
William de Vazelhes <william.de-vazelhes@inria.fr>
2019-06-25 08:50:12 +0200

    let's try again with 30 samples

--- test/test_sklearn_compat.py
+++ test/test_sklearn_compat.py
@@ -136,7 +136,7 @@ def test_array_like_inputs(estimator, build_dataset, with_preprocessor):
 
   # we subsample the data for the test to be more efficient
   input_data, _, labels, _ = train_test_split(input_data, labels,
-                                              train_size=20)
+                                              train_size=30)
   X = X[:10]
 
   estimator = clone(estimator)
