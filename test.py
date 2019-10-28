1e2f6b9b22652c5d951dd345f11cfe599662d39e
Robin Vogel <robvogel0@gmail.com>
2019-10-28 15:07:28 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
-from metric_learn import LMNN
-
-X, y = load_iris(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-model = LMNN(verbose=True)
-model.fit(X_train, y_train)
