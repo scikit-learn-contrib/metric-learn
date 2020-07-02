d218d78365b8355ef5beadf126a6ed5accb6277f
William de Vazelhes <31916524+wdevazelhes@users.noreply.github.com>
2020-07-02 14:13:07 +0200

    [MRG] Add python requires plus message for old pips (#299)
    * Add python requires plus message for old pips
    
    * Update install requires for scikit-learn

--- setup.py
+++ setup.py
@@ -3,6 +3,32 @@
 from setuptools import setup
 import os
 import io
+import sys
+
+
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (3, 6)
+
+# This check and everything above must remain compatible with Python 2.7.
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    sys.stderr.write("""
+==========================
+Unsupported Python version
+==========================
+This version of metric-learn requires Python {}.{}, but you're trying to
+install it on Python {}.{}.
+This may be because you are using a version of pip that doesn't
+understand the python_requires classifier. Make sure you
+have pip >= 9.0 and setuptools >= 24.2, then try again:
+    $ python -m pip install --upgrade pip setuptools
+    $ python -m pip install django
+This will install the latest version of metric-learn which works on your
+version of Python. If you can't upgrade your pip (or Python), request
+an older version of metric-learn:
+    $ python -m pip install "metric-learn<0.6.0"
+""".format(*(REQUIRED_PYTHON + CURRENT_PYTHON)))
+    sys.exit(1)
+
 
 version = {}
 with io.open(os.path.join('metric_learn', '_version.py')) as fp:
@@ -16,6 +42,7 @@ setup(name='metric-learn',
       version=version['__version__'],
       description='Python implementations of metric learning algorithms',
       long_description=long_description,
+      python_requires='>={}.{}'.format(*REQUIRED_PYTHON),
       author=[
           'CJ Carey',
           'Yuan Tang',
@@ -38,7 +65,7 @@ setup(name='metric-learn',
       install_requires=[
           'numpy',
           'scipy',
-          'scikit-learn',
+          'scikit-learn>=0.20.3',
       ],
       extras_require=dict(
           docs=['sphinx', 'shinx_rtd_theme', 'numpydoc'],
