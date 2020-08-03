eb878d6f42bb71f8f9ef7ac16eed108c9c3b6ecf
CJ Carey <perimosocordiae@gmail.com>
2020-08-03 17:03:45 -0400

    Resolve a numpy deprecation warning

--- test/test_utils.py
+++ test/test_utils.py
@@ -931,16 +931,17 @@ def test_same_with_or_without_preprocessor(estimator, build_dataset):
       assert np.array(output_with_prep == output_with_prep_formed).all()
 
   # test score_pairs
+  idx1 = np.array([[0, 2],[5, 3]], dtype=int)
   output_with_prep = estimator_with_preprocessor.score_pairs(
-      indicators_to_transform[[[[0, 2], [5, 3]]]])
+      indicators_to_transform[idx1])
   output_without_prep = estimator_without_preprocessor.score_pairs(
-      formed_points_to_transform[[[[0, 2], [5, 3]]]])
+      formed_points_to_transform[idx1])
   assert np.array(output_with_prep == output_without_prep).all()
 
   output_with_prep = estimator_with_preprocessor.score_pairs(
-      indicators_to_transform[[[[0, 2], [5, 3]]]])
+      indicators_to_transform[idx1])
   output_without_prep = estimator_with_prep_formed.score_pairs(
-      formed_points_to_transform[[[[0, 2], [5, 3]]]])
+      formed_points_to_transform[idx1])
   assert np.array(output_with_prep == output_without_prep).all()
 
   # test transform
