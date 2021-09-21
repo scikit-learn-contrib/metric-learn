e61038465e8076518d72e3e7bddf3212ca23b5d3
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2021-09-21 14:05:39 +0200

    Fix identation

--- metric_learn/oasis.py
+++ metric_learn/oasis.py
@@ -1,10 +1,34 @@
-from .base_metric import BilinearMixin
+from .base_metric import BilinearMixin, _TripletsClassifierMixin
 import numpy as np
 
-class OASIS(BilinearMixin):
 
-    def __init__(self, preprocessor=None):
+class OASIS(BilinearMixin, _TripletsClassifierMixin):
+    """
+    Key params:
+
+    max_iter: Max number of iterations. Can differ from n_samples
+
+    c: Passive-agressive param. Controls trade-off bewteen remaining
+         close to previous W_i-1 OR minimizing loss of current triplet
+
+    seed: For random sampling
+
+    shuffle: If True will shuffle the triplets given to fit.
+    """
+
+    def __init__(
+            self,
+            preprocessor=None,
+            max_iter=10,
+            c=1e-6,
+            random_seed=33,
+            shuffle=False):
         super().__init__(preprocessor=preprocessor)
+        self.components_ = None  # W matrix
+        self.d = 0  # n_features
+        self.max_iter = max_iter  # Max iterations
+        self.c = c  # Trade-off param
+        self.random_state = random_seed  # RNG
 
     def fit(self, X, y):
         """
@@ -16,7 +40,8 @@ class OASIS(BilinearMixin):
         y : (n) data labels
         """
         X = self._prepare_inputs(X, y, ensure_min_samples=2)
-        
+
         # Dummy fit
-        self.components_ = np.random.rand(np.shape(X[0])[-1], np.shape(X[0])[-1])
+        self.components_ = np.random.rand(
+            np.shape(X[0])[-1], np.shape(X[0])[-1])
         return self
--- oasis.py
+++ oasis.py
@@ -1,15 +1,16 @@
-from metric_learn.oasis import OASIS
 import numpy as np
-from numpy.random.mtrand import random_integers, seed
 from sklearn.utils import check_random_state
+
 
 class _BaseOASIS():
     """
     Key params:
 
     n_iter: Can differ from n_samples
-    c: passive-agressive param. Controls trade-off bewteen remaining close to previous W_i-1 OR minimizing loss of current triplet
-    seed: For random sampling
+    c:      passive-agressive param. Controls trade-off bewteen
+            remaining close to previous W_i-1 OR minimizing loss of
+            current triplet.
+    seed:   For random sampling
     """
 
     def __init__(self, n_iter=10, c=1e-6, seed=33) -> None:
@@ -18,7 +19,6 @@ class _BaseOASIS():
         self.n_iter = n_iter
         self.c = c
         self.random_state = seed
-        
 
     def _fit(self, triplets):
         """
@@ -27,13 +27,17 @@ class _BaseOASIS():
         Matrix W is already defined as I at __init___
         """
         self.d = np.shape(triplets)[2]       # Number of features
-        self.components_ = np.identity(self.d) if self.components_ is None else self.components_  # W_0 = I, Here and once, to reuse self.components_ for partial_fit
+        # W_0 = I, Here and once, to reuse self.components_ for partial_fit
+        self.components_ = np.identity(
+            self.d) if self.components_ is None else self.components_
 
         n_triplets = np.shape(triplets)[0]
         rng = check_random_state(self.random_state)
 
         # Gen n_iter random indices
-        random_indices = rng.randint(low=0, high=n_triplets, size=(self.n_iter))
+        random_indices = rng.randint(
+            low=0, high=n_triplets, size=(
+                self.n_iter))
 
         # TODO: restict n_iter >, < or = to n_triplets
         i = 0
@@ -42,20 +46,20 @@ class _BaseOASIS():
             loss = self._loss(current_triplet)
             vi = self._vi_matrix(current_triplet)
             fs = self._frobenius_squared(vi)
-            tau_i = np.minimum(self.c, loss / fs) # Global GD or Adjust to tuple
+            # Global GD or Adjust to tuple
+            tau_i = np.minimum(self.c, loss / fs)
 
             # Update components
             self.components_ = np.add(self.components_, tau_i * vi)
             print(self.components_)
 
             i = i + 1
-    
+
     def _partial_fit(self, new_triplets):
         """
         self.components_ already defined, we reuse previous fit
         """
         self._fit(new_triplets)
-
 
     def _frobenius_squared(self, v):
         """
@@ -75,29 +79,30 @@ class _BaseOASIS():
 
         It uses self.components_ as current matrix W
         """
-        return np.diagonal(np.dot(np.dot(pairs[:, 0, :], self.components_), pairs[:, 1, :].T))
-
+        return np.diagonal(
+            np.dot(np.dot(pairs[:, 0, :], self.components_), pairs[:, 1, :].T))
 
     def _loss(self, triplet):
         """
         Loss function in a triplet
         """
-        return np.maximum(0, 1 - self._score_pairs(np.array([ [triplet[0], triplet[1]], ]))[0] + self._score_pairs(np.array([ [triplet[0], triplet[2]], ]))[0] )
-    
+        return np.maximum(0, 1 -
+                          self._score_pairs([[triplet[0], triplet[1]], ])[0] +
+                          self._score_pairs([[triplet[0], triplet[2]], ])[0])
 
     def _vi_matrix(self, triplet):
         """
         Computes V_i, the gradient matrix in a triplet
         """
         # (pi+ - pi-)
-        diff = np.subtract(triplet[1], triplet[2]) # Shape (, d)
+        diff = np.subtract(triplet[1], triplet[2])  # Shape (, d)
         result = []
 
         # For each scalar in first triplet, multiply by the diff of pi+ and pi-
         for v in triplet[0]:
-            result.append( v * diff)
+            result.append(v * diff)
 
-        return np.array(result) # Shape (d, d)
+        return np.array(result)  # Shape (d, d)
 
 
 def test_OASIS():
@@ -105,13 +110,14 @@ def test_OASIS():
                          [[2, 1], [0, 1], [2, 0]],
                          [[0, 0], [2, 0], [0, 1]],
                          [[2, 0], [0, 0], [2, 1]]])
-    
+
     oasis = _BaseOASIS(n_iter=2, c=0.24, seed=33)
     oasis._fit(triplets)
 
     new_triplets = np.array([[[0, 1], [4, 5], [0, 0]],
-                            [[2,0], [4, 7], [2, 0]]])
+                            [[2, 0], [4, 7], [2, 0]]])
 
     oasis._partial_fit(new_triplets)
+
 
 test_OASIS()
