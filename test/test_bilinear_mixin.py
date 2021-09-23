edf64c5c2f5a2b544cf15b64fea579da786b4c38
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2021-09-23 10:44:41 +0200

    Commented each test

--- test/test_bilinear_mixin.py
+++ test/test_bilinear_mixin.py
@@ -16,23 +16,42 @@ class IdentityBilinearMixin(BilinearMixin):
     super().__init__(preprocessor=preprocessor)
 
   def fit(self, X, y):
+    """
+    Checks input's format. Sets M matrix to identity of shape (d,d)
+    where d is the dimension of the input.
+    """
     X, y = self._prepare_inputs(X, y, ensure_min_samples=2)
     self.d = np.shape(X[0])[-1]
     self.components_ = np.identity(self.d)
     return self
 
   def random_M(self):
+    """
+    Changes the matrix M for a random one of shape (d,d)
+    """
     self.components_ = np.random.rand(self.d, self.d)
 
 
-def test_same_similarity_with_two_methods():
+def identity_fit(d=100):
+  """
+  Creates two d-dimentional arrays. Fits an IdentityBilinearMixin()
+  and then returns the two arrays and the mixin. Testing purposes
+  """
   d = 100
   u = np.random.rand(d)
   v = np.random.rand(d)
   mixin = IdentityBilinearMixin()
   mixin.fit([u, v], [0, 0])
-  mixin.random_M()  # Dummy fit
+  return u, v, mixin
 
+
+def test_same_similarity_with_two_methods():
+  """"
+  Tests that score_pairs() and get_metric() give consistent results.
+  In both cases, the results must match for the same input.
+  """
+  u, v, mixin = identity_fit()
+  mixin.random_M()  # Dummy fit
   # The distances must match, whether calc with get_metric() or score_pairs()
   dist1 = mixin.score_pairs([[u, v], [v, u]])
   dist2 = [mixin.get_metric()(u, v), mixin.get_metric()(v, u)]
@@ -41,11 +60,13 @@ def test_same_similarity_with_two_methods():
 
 
 def test_check_correctness_similarity():
+  """
+  Tests the correctness of the results made from socre_paris() and
+  get_metric(). Results are compared with the real bilinear similarity
+  calculated in-place.
+  """
   d = 100
-  u = np.random.rand(d)
-  v = np.random.rand(d)
-  mixin = IdentityBilinearMixin()
-  mixin.fit([u, v], [0, 0])  # Identity fit
+  u, v, mixin = identity_fit(d)
   dist1 = mixin.score_pairs([[u, v], [v, u]])
   dist2 = [mixin.get_metric()(u, v), mixin.get_metric()(v, u)]
 
@@ -57,6 +78,10 @@ def test_check_correctness_similarity():
 
 
 def test_check_handmade_example():
+  """
+  Checks that score_pairs() result is correct comparing it with a
+  handmade example.
+  """
   u = np.array([0, 1, 2])
   v = np.array([3, 4, 5])
   mixin = IdentityBilinearMixin()
@@ -68,6 +93,11 @@ def test_check_handmade_example():
 
 
 def test_check_handmade_symmetric_example():
+  """
+  When the Bilinear matrix is the identity. The similarity
+  between two arrays must be equal: S(u,v) = S(v,u). Also
+  checks the random case: when the matrix is pd and symetric.
+  """
   u = np.array([0, 1, 2])
   v = np.array([3, 4, 5])
   mixin = IdentityBilinearMixin()
@@ -77,11 +107,13 @@ def test_check_handmade_symmetric_example():
 
 
 def test_score_pairs_finite():
+  """
+  Checks for 'n' score_pairs() of 'd' dimentions, that all
+  similarities are finite numbers, not NaN, +inf or -inf.
+  Considering a random M for bilinear similarity.
+  """
   d = 100
-  u = np.random.rand(d)
-  v = np.random.rand(d)
-  mixin = IdentityBilinearMixin()
-  mixin.fit([u, v], [0, 0])
+  u, v, mixin = identity_fit(d)
   mixin.random_M()  # Dummy fit
   n = 100
   X = np.array([np.random.rand(d) for i in range(n)])
@@ -90,14 +122,13 @@ def test_score_pairs_finite():
 
 
 def test_score_pairs_dim():
-  # scoring of 3D arrays should return 1D array (several tuples),
-  # and scoring of 2D arrays (one tuple) should return an error (like
-  # scikit-learn's error when scoring 1D arrays)
+  """
+  Scoring of 3D arrays should return 1D array (several tuples),
+  and scoring of 2D arrays (one tuple) should return an error (like
+  scikit-learn's error when scoring 1D arrays)
+  """
   d = 100
-  u = np.random.rand(d)
-  v = np.random.rand(d)
-  mixin = IdentityBilinearMixin()
-  mixin.fit([u, v], [0, 0])
+  u, v, mixin = identity_fit()
   mixin.random_M()  # Dummy fit
   n = 100
   X = np.array([np.random.rand(d) for i in range(n)])
@@ -113,11 +144,10 @@ def test_score_pairs_dim():
 
 
 def test_check_scikitlearn_compatibility():
+  """Check that the similarity returned by get_metric() is compatible with
+  scikit-learn's algorithms using a custom metric, DBSCAN for instance"""
   d = 100
-  u = np.random.rand(d)
-  v = np.random.rand(d)
-  mixin = IdentityBilinearMixin()
-  mixin.fit([u, v], [0, 0])
+  u, v, mixin = identity_fit(d)
   mixin.random_M()  # Dummy fit
 
   n = 100
