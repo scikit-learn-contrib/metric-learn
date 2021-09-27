7843c0b8c96cf8ec4d4c883a0eb996b134bf6855
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2021-09-27 16:28:41 +0200

    Expected warning silenced

--- test/test_similarity_learn.py
+++ test/test_similarity_learn.py
@@ -10,6 +10,9 @@ def test_sanity_check():
   """
   With M=I init. As the algorithm sees more triplets,
   the score(triplet) should increse or maintain.
+
+  A warning might show up regarding division by 0. See
+  test_divide_zero for further research.
   """
   triplets = np.array([[[0, 1], [2, 1], [0, 0]],
                        [[2, 1], [0, 1], [2, 0]],
@@ -21,25 +24,27 @@ def test_sanity_check():
   oasis1.fit(triplets)
   a1 = oasis1.score(triplets)
 
+  msg = "divide by zero encountered in double_scalars"
+  with pytest.warns(RuntimeWarning) as raised_warning:
   # See 2/4 triplets
-  oasis2 = OASIS(max_iter=2, c=0.24, random_state=RNG)
-  oasis2.fit(triplets)
-  a2 = oasis2.score(triplets)
+    oasis2 = OASIS(max_iter=2, c=0.24, random_state=RNG)
+    oasis2.fit(triplets)
+    a2 = oasis2.score(triplets)
 
-  # See 3/4 triplets
-  oasis3 = OASIS(max_iter=3, c=0.24, random_state=RNG)
-  oasis3.fit(triplets)
-  a3 = oasis3.score(triplets)
+    # See 3/4 triplets
+    oasis3 = OASIS(max_iter=3, c=0.24, random_state=RNG)
+    oasis3.fit(triplets)
+    a3 = oasis3.score(triplets)
 
-  # See 5/4 triplets, one is seen again
-  oasis4 = OASIS(max_iter=6, c=0.24, random_state=RNG)
-  oasis4.fit(triplets)
-  a4 = oasis4.score(triplets)
+    # See 5/4 triplets, one is seen again
+    oasis4 = OASIS(max_iter=6, c=0.24, random_state=RNG)
+    oasis4.fit(triplets)
+    a4 = oasis4.score(triplets)
 
-  assert a2 >= a1
-  assert a3 >= a2
-  assert a4 >= a3
-
+    assert a2 >= a1
+    assert a3 >= a2
+    assert a4 >= a3
+  assert msg == raised_warning[0].message.args[0]
 
 def test_score_zero():
   """
