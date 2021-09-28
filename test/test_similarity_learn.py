ab77a054d577d818528e5332d9d45f46d5ca3970
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2021-09-28 13:05:11 +0200

    Generalized test_indices_funct test

--- test/test_similarity_learn.py
+++ test/test_similarity_learn.py
@@ -86,8 +86,10 @@ def test_divide_zero():
     oasis1.fit(triplets)
   assert msg == raised_warning[0].message.args[0]
 
-
-def test_indices_funct():
+@pytest.mark.parametrize(('n_triplets', 'n_iter'),
+                           [(10, 10), (33, 70), (100, 67),
+                           (10000, 20000)])
+def test_indices_funct(n_triplets, n_iter):
   """
   This test verifies the behaviour of _get_random_indices. The
   method used inside OASIS that defines the order in which the
@@ -95,93 +97,92 @@ def test_indices_funct():
   """
   oasis = OASIS(random_state=SEED)
   # Not random cases
-  base = np.arange(20)
+  base = np.arange(n_triplets)
 
   # n_iter = n_triplets
+  if n_iter == n_triplets:
+    r = oasis._get_random_indices(n_triplets=n_triplets, n_iter=n_iter,
+                                  shuffle=False, random=False)
+    assert_array_equal(r, base)  # No shuffle
+    assert len(r) == len(base)  # Same lenght
 
-  r = oasis._get_random_indices(n_triplets=20, n_iter=20,
-                                shuffle=False, random=False)
-  assert_array_equal(r, base)  # No shuffle
-  assert len(r) == len(base)  # Same lenght
-
-  # Shuffle
-  r = oasis._get_random_indices(n_triplets=20, n_iter=20,
-                                shuffle=True, random=False)
-  with assert_raises(AssertionError):  # Should be different
-    assert_array_equal(r, base)
-  # But contain the same elements
-  assert_array_equal(np.unique(r), np.unique(base))
-  assert len(r) == len(base)  # Same lenght
+    # Shuffle
+    r = oasis._get_random_indices(n_triplets=n_triplets, n_iter=n_iter,
+                                  shuffle=True, random=False)
+    with assert_raises(AssertionError):  # Should be different
+      assert_array_equal(r, base)
+    # But contain the same elements
+    assert_array_equal(np.unique(r), np.unique(base))
+    assert len(r) == len(base)  # Same lenght
 
   # n_iter > n_triplets
+  if n_iter > n_triplets:
+    r = oasis._get_random_indices(n_triplets=n_triplets, n_iter=n_iter,
+                                  shuffle=False, random=False)
+    assert_array_equal(r[:n_triplets], base)  # First n_triplets must match
+    assert len(r) == n_iter  # Expected lenght
 
-  r = oasis._get_random_indices(n_triplets=20, n_iter=40,
-                                shuffle=False, random=False)
-  assert_array_equal(r[:20], base)  # First n_triplets must match
-  assert len(r) == 40  # Expected lenght
+    # Next n_iter-n_triplets must be in range(n_triplets)
+    sample = r[n_triplets:]
+    for i in range(n_iter - n_triplets):
+      if sample[i] not in base:
+        raise AssertionError("Sampling has values out of range")
 
-  # Next n_iter-n_triplets must be in range(n_triplets)
-  sample = r[20:]
-  for i in range(40 - 20):
-    if sample[i] not in base:
-      raise AssertionError("Sampling has values out of range")
-
-  # Shuffle
-  r = oasis._get_random_indices(n_triplets=20, n_iter=40,
-                                shuffle=True, random=False)
-  assert len(r) == 40  # Expected lenght
-  # Each triplet must be at least one time
-  assert_array_equal(np.unique(r), np.unique(base))
-  with assert_raises(AssertionError):  # First 20 should be different
-    assert_array_equal(r[:20], base)
+    # Shuffle
+    r = oasis._get_random_indices(n_triplets=n_triplets, n_iter=n_iter,
+                                  shuffle=True, random=False)
+    assert len(r) == n_iter  # Expected lenght
+    # Each triplet must be at least one time
+    assert_array_equal(np.unique(r), np.unique(base))
+    with assert_raises(AssertionError):  # First n_triplets should be different
+      assert_array_equal(r[:n_triplets], base)
 
   # n_iter < n_triplets
+  if n_iter < n_triplets:
+    r = oasis._get_random_indices(n_triplets=n_triplets, n_iter=n_iter,
+                                  shuffle=False, random=False)
+    assert len(r) == n_iter  # Expected lenght
+    u = np.unique(r)
+    assert len(u) == len(r)  # No duplicates
+    # Final array must cointain only elements in range(n_triplets)
+    for i in range(n_iter):
+      if r[i] not in base:
+        raise AssertionError("Sampling has values out of range")
 
-  r = oasis._get_random_indices(n_triplets=20, n_iter=10,
-                                shuffle=False, random=False)
-  assert len(r) == 10  # Expected lenght
-  u = np.unique(r)
-  assert len(u) == len(r)  # No duplicates
-  # Final array must cointain only elements in range(n_triplets)
-  for i in range(10):
-    if r[i] not in base:
-      raise AssertionError("Sampling has values out of range")
+    # Shuffle must have no efect
+    oasis_a = OASIS(random_state=SEED)
+    r_a = oasis_a._get_random_indices(n_triplets=n_triplets, n_iter=n_iter,
+                                      shuffle=False, random=False)
 
-  # Shuffle must have no efect
-  oasis_a = OASIS(random_state=SEED)
-  r_a = oasis_a._get_random_indices(n_triplets=20, n_iter=10,
-                                    shuffle=False, random=False)
-
-  oasis_b = OASIS(random_state=SEED)
-  r_b = oasis_b._get_random_indices(n_triplets=20, n_iter=10,
-                                    shuffle=True, random=False)
-  assert_array_equal(r_a, r_b)
+    oasis_b = OASIS(random_state=SEED)
+    r_b = oasis_b._get_random_indices(n_triplets=n_triplets, n_iter=n_iter,
+                                      shuffle=True, random=False)
+    assert_array_equal(r_a, r_b)
 
   # Random case
   # n_iter = n_triplets
-  r = oasis._get_random_indices(n_triplets=20, n_iter=20, random=True)
-  assert len(r) == 20  # Expected lenght
-  for i in range(20):
-    if r[i] not in base:
-      raise AssertionError("Sampling has values out of range")
-  # n_iter > n_triplets
-  r = oasis._get_random_indices(n_triplets=20, n_iter=40, random=True)
-  assert len(r) == 40  # Expected lenght
-  for i in range(40):
-    if r[i] not in base:
-      raise AssertionError("Sampling has values out of range")
-  # n_iter < n_triplets
-  r = oasis._get_random_indices(n_triplets=20, n_iter=10, random=True)
-  assert len(r) == 10  # Expected lenght
-  for i in range(10):
+  r = oasis._get_random_indices(n_triplets=n_triplets, n_iter=n_iter, random=True)
+  assert len(r) == n_iter  # Expected lenght
+  for i in range(n_iter):
     if r[i] not in base:
       raise AssertionError("Sampling has values out of range")
   # Shuffle has no effect
   oasis_a = OASIS(random_state=SEED)
-  r_a = oasis_a._get_random_indices(n_triplets=20, n_iter=10,
+  r_a = oasis_a._get_random_indices(n_triplets=n_triplets, n_iter=n_iter,
                                     shuffle=False, random=True)
 
   oasis_b = OASIS(random_state=SEED)
-  r_b = oasis_b._get_random_indices(n_triplets=20, n_iter=10,
+  r_b = oasis_b._get_random_indices(n_triplets=n_triplets, n_iter=n_iter,
                                     shuffle=True, random=True)
   assert_array_equal(r_a, r_b)
+
+  # n_triplets and n_iter cannot be 0
+  msg = ("n_triplets cannot be 0")
+  with pytest.raises(ValueError) as raised_error:
+    oasis._get_random_indices(n_triplets=0, n_iter=n_iter, random=True)
+  assert msg == raised_error.value.args[0]
+
+  msg = ("n_iter cannot be 0")
+  with pytest.raises(ValueError) as raised_error:
+    oasis._get_random_indices(n_triplets=n_triplets, n_iter=0, random=True)
+  assert msg == raised_error.value.args[0]
