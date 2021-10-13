dde35760bed45165f9bc7cd052d50b2685347189
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2021-10-13 16:20:25 +0200

    Add more testing. Test refactor TBD

--- test/test_bilinear_mixin.py
+++ test/test_bilinear_mixin.py
@@ -69,19 +69,22 @@ def test_same_similarity_with_two_methods():
 
 def test_check_correctness_similarity():
   """
-  Tests the correctness of the results made from socre_paris() and
-  get_metric(). Results are compared with the real bilinear similarity
-  calculated in-place.
+  Tests the correctness of the results made from socre_paris(),
+  get_metric() and get_bilinear_matrix. Results are compared with
+  the real bilinear similarity calculated in-place.
   """
   d, n, n_pairs = 100, 100, 1000
   _, random_pairs, mixin = identity_fit(d=d, n=n, n_pairs=n_pairs, random=True)
   dist1 = mixin.pair_similarity(random_pairs)
   dist2 = [mixin.get_metric()(p[0], p[1]) for p in random_pairs]
+  dist3 = [np.dot(np.dot(p[0].T, mixin.get_bilinear_matrix()), p[1])
+           for p in random_pairs]
   desired = [np.dot(np.dot(p[0].T, mixin.components_), p[1])
              for p in random_pairs]
 
   assert_array_almost_equal(dist1, desired)  # pair_similarity
   assert_array_almost_equal(dist2, desired)  # get_metric
+  assert_array_almost_equal(dist3, desired)  # get_metric
 
 
 def test_check_handmade_example():
@@ -153,9 +156,43 @@ def test_pair_similarity_dim():
 
 
 def test_check_scikitlearn_compatibility():
-  """Check that the similarity returned by get_metric() is compatible with
-  scikit-learn's algorithms using a custom metric, DBSCAN for instance"""
+  """
+  Check that the similarity returned by get_metric() is compatible with
+  scikit-learn's algorithms using a custom metric, DBSCAN for instance
+  """
   d, n = 100, 100
   X, _, mixin = identity_fit(d=d, n=n, n_pairs=None, random=True)
   clustering = DBSCAN(metric=mixin.get_metric())
   clustering.fit(X)
+
+
+def test_check_score_pairs_deprecation_and_output():
+  """
+  Check that calling score_pairs shows a warning of deprecation, and also
+  that the output of score_pairs matches calling pair_similarity.
+  """
+  d, n, n_pairs = 100, 100, 1000
+  _, random_pairs, mixin = identity_fit(d=d, n=n, n_pairs=n_pairs, random=True)
+  dpr_msg = ("score_pairs will be deprecated in release 0.6.4. "
+             "Use pair_similarity to compute similarities, or "
+             "pair_distances to compute distances.")
+  with pytest.warns(FutureWarning) as raised_warnings:
+    s1 = mixin.score_pairs(random_pairs)
+    s2 = mixin.pair_similarity(random_pairs)
+    assert_array_almost_equal(s1, s2)
+  assert any(str(w.message) == dpr_msg for w in raised_warnings)
+
+
+def test_check_error_with_pair_distance():
+  """
+  Check that calling pair_distance is not possible with a Bilinear learner.
+  An Exception must be shown instead.
+  """
+  d, n, n_pairs = 100, 100, 1000
+  _, random_pairs, mixin = identity_fit(d=d, n=n, n_pairs=n_pairs, random=True)
+  msg = ("Bilinear similarity learners don't learn a distance, thus ",
+         "this method is not implemented. Use pair_similarity to "
+         "compute similarity between pairs")
+  with pytest.raises(Exception) as e:
+    _ = mixin.pair_distance(random_pairs)
+  assert e.value.args[0] == msg
