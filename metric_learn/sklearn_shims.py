0acf8b1ec746809dfeef2207a6f29b41de122a0f
CJ Carey <perimosocordiae@gmail.com>
2023-09-28 09:03:44 -0400

    Remove deprecated assert_warns_message import
    This was deprecated and is now removed from sklearn. Thankfully,
    we don't appear to be using it.

--- metric_learn/sklearn_shims.py
+++ metric_learn/sklearn_shims.py
@@ -6,7 +6,6 @@ SKLEARN_AT_LEAST_0_22 = (version.parse(sklearn.__version__)
                          >= version.parse('0.22.0'))
 if SKLEARN_AT_LEAST_0_22:
     from sklearn.utils._testing import (set_random_state,
-                                        assert_warns_message,
                                         ignore_warnings,
                                         assert_allclose_dense_sparse,
                                         _get_args)
@@ -15,13 +14,12 @@ if SKLEARN_AT_LEAST_0_22:
     from sklearn.metrics._scorer import get_scorer
 else:
     from sklearn.utils.testing import (set_random_state,
-                                       assert_warns_message,
                                        ignore_warnings,
                                        assert_allclose_dense_sparse,
                                        _get_args)
     from sklearn.utils.estimator_checks import is_public_parameter
     from sklearn.metrics.scorer import get_scorer
 
-__all__ = ['set_random_state', 'assert_warns_message', 'set_random_state',
+__all__ = ['set_random_state', 'set_random_state',
            'ignore_warnings', 'assert_allclose_dense_sparse', '_get_args',
            'is_public_parameter', 'get_scorer']
